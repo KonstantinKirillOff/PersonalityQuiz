79349ea37cf66c7c9b427377b706a8bcbbc33525
KonstantinKirillOff <53314883+KonstantinKirillOff@users.noreply.github.com>
2021-09-04 13:22:47 +0300

    increase logic after review HW

--- PersonalityQuiz/ViewControllers/ResultViewController.swift
+++ PersonalityQuiz/ViewControllers/ResultViewController.swift
@@ -17,28 +17,33 @@ class ResultViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.hidesBackButton = true
+        
         guard  let answers = answers else { return }
         let resultAnimal = getResult(from: answers)
         resultAnimalLabel.text = "Вы - \(resultAnimal.rawValue)"
         descriptionAnimalLabel.text = resultAnimal.definition
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        navigationItem.hidesBackButton = true
-    }
+
 }
 
 // MARK: - Private Methods
 extension ResultViewController {
     
     private func getResult(from answers: [Answer]) -> Animal {
-        let totalAnswers = answers.map { $0.animal }
-                                  .reduce(into: [:]) {
-                                            counts,  animalInAnswer in counts[animalInAnswer, default: 0] += 1 }
-                                  .sorted(by: {
-                                            $0.value > $1.value })
-            
-        return totalAnswers[0].key
+        //Решение № 1
+        //        let totalAnswers = answers.map { $0.animal }
+        //            .reduce(into: [:]) { counts,  animalInAnswer in counts[animalInAnswer, default: 0] += 1 }
+        //            .sorted(by: { $0.value > $1.value })
+        //
+        //        return totalAnswers[0].key
+        
+        //Решение №2
+        let mostFrequencyAnimal = Dictionary(grouping: answers) { $0.animal }
+            .sorted{ $0.value.count > $1.value.count }
+            .first?.key
+        
+        return mostFrequencyAnimal!
     }
     
 }
